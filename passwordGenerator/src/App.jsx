8c901b1ae0d309e58809b563562ae3fe48a5a865
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-07-07 12:57:01 +0530

    Made notes on useCallBack, useRef, useEffect , Memoization,

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -17,15 +17,23 @@ const [password,setPassword] = useState("");
 
 //5.useRef() hook:-
 //What is useRef hook in react?
-// useRef hook in react its responsible for creating a reference to the Dom directly within the functional component
+//useRef hook in react its responsible for creating a reference to the Dom directly within the functional component.
 const passwordRef = useRef(null);
 
 ///2.Lets create a 'passwordGenerator' method for optimization and memoization concept
 //Because this is an one function or method we could say we are using for several times.
 //So in the React there is something known as 'useCallBack()' hook for memoization concept and we are using for optimization performance
 //So what is useCallBack() hook, useCallBack hook is a React hook that allow cache a function definition between re-renders.
-//what is cache means its for memory or storage and when we needed we can request them.
+//what is cache means cache its like a plain object , caching is a technique used in react it helps to store a copy of a given resource in our browser and serves it back when it is requested.
 //Cache is just a plain object,caching is a technique used in react that helps to store the copy of a given resource in our browser and serves it back when needed or requested.
+
+//What is Memoization:-
+// Memoization its a smart way programming technique that we used  to make our functions faster by storing and reusing their past results and it relies on the idea of cache which is like a browsers memory storage.
+
+
+//What is callBack function:-
+//callBack function is a function that is passed as an argument to another function and then is executed when another function has finised execution.
+
 
 
 //Logic for passwordGenerator:-
@@ -52,12 +60,27 @@ const copyPassWordToClipboard =  useCallback(()=>{
 window.navigator.clipboard.writeText(password);
 },[password])
 
-//useEffect hook in react user for performing sideEffects in functional component, with the help of useEffect hook it has something to be done after rendering the component or after a state change, we can used in data fetching,manipulating the DOM ,it accepts two arguments the first is callBack function and the second is an optional dependency array.
-//Now what are sideffects in react, sideEffects refer to any operations or behaviors that occurs in a component after rendering and that dont directly impact the current component render cycle.these sideeffects can be such as data fetching,interactions with the outer world and manipulating the DOM.
 
+//What is useEffect hook():-
+//useEffect hook in react is used for performing sideEffects in functional components, 
+//with the help of useEffect hook it has something to be done after rendering the component or after a state change, we can used in data fetching,manipulating the DOM ,it accepts two arguments the first is callBack function and the second is an optional dependency array.
+//Now what are sideffects in react, sideEffects refer to any operations or behaviors that occurs in a component after rendering it ,these sideeffects can be such as data fetching,interactions with the outer world and manipulating the DOM.
+
+
+//Dependency array in useEffect hook:-
+//1.No dependency array:-
+//Which means that the effect will run after every render which can lead to performance issues.
+
+//2.Empty dependency array:-
+//Which means that it will render only once after initial render.
+
+//3.Dependency array filled with dependencies:-
+//Which means that the effect runs when there is change in the dependency.
 useEffect(()=>{
 passwordGenerator();
 },[length,numbersAllowed,charsAllowed,passwordGenerator])
+
+
   return (
     <div className=' w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 '>
       <h1 className='text-white text-center my-3'>Password generator</h1>
@@ -87,6 +110,7 @@ passwordGenerator();
       className='cursor-pointer'
       value={length}
       //onChange Event handler:-
+      //In React onChange event handler is used to it handles and triggers the action when there is chang in the input field.
       onChange={(event)=> setLength(event.target.value)}
     />
   <label htmlFor='rangeLength'>Length: {length}</label>
@@ -97,7 +121,7 @@ passwordGenerator();
         defaultChecked={numbersAllowed}
         id='numberInput'
         //setState function can also accepts a callBack function that can take previous values.
-        //so here the logic is 'prev' that means by default of numbersAllowed is false so when the user clicks on a check button its true so here the prev value is false then not operator means ooposite true converted.
+        //so here the logic is 'prev' that means by default of numbersAllowed is false so when the user clicks on a check button its true so here the prev value is false then not operator means opposite true converted.
         onChange={()=> setNumbersAllowed((prev)=> !prev)}
       />
         <label htmlFor="numberInput">Numbers</label>
