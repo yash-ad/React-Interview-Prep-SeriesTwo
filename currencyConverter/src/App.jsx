fa58de8345e3d754ab3fb92419b22774ba3459ce
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-07-08 22:00:57 +0530

    New file and folder for currencyConverter

---
+++ currencyConverter/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+   <h1 className=" flex text-white text-3xl text-center ">Currency Converter</h1>
+  )
+}
+
+export default App
---
+++ currencyConverter/src/CustomHooks/useCurrencyInfo.js
@@ -0,0 +1,22 @@
+import {useEffect,useState} from 'react'
+
+
+
+function useCurrencyInfo (currency){
+    const[data,setData]= useState({});
+
+    
+    //When component mounts that means the reacts lifecycle triggers the method when the after component is rendered to the Dom.
+//In functional component useEffect for handling sideEffects.
+//To make an API calls
+   useEffect(() => {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
+    )
+    .then((response)=> response.json())
+    .then((responseData)=> setData(responseData[currency]))
+    .catch((error)=> console.error(error,"Error Found"))
+   }, [currency]);
+return data;
+}
+
+export default useCurrencyInfo;
