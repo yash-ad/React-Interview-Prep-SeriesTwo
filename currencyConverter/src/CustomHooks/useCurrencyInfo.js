be87f56816274d2b3ef60f96a8578c585d06fb8c
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-07-09 17:53:40 +0530

    currency converter project completed

--- currencyConverter/src/CustomHooks/useCurrencyInfo.js
+++ currencyConverter/src/CustomHooks/useCurrencyInfo.js
@@ -4,11 +4,10 @@ import {useEffect,useState} from 'react'
 
 function useCurrencyInfo (currency){
     const[data,setData]= useState({});
-
     
     //When component mounts that means the reacts lifecycle triggers the method when the after component is rendered to the Dom.
 //In functional component useEffect for handling sideEffects.
-//To make an API calls
+//To make an API calls.
    useEffect(() => {
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`
     )
---
+++ currencyConverter/src/components/inputBox.jsx
@@ -0,0 +1,62 @@
+import { useId } from "react";
+
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
+}) {
+   
+//Lets use 'useId()' hook for better performance.
+
+const amountInputId = useId();
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
+                   {label}
+                </label>
+                <input
+                    className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    id={amountInputId}
+                    // If there is function available onAmountChange and call the function onAmountChange().
+                    //Sometimes the javascript from an event object values is formatted to the string thats why we have to convert into the Number()
+                    onChange={(event)=> onAmountChange && onAmountChange(Number(event.target.value))}
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(event)=> onCurrencyChange && onCurrencyChange(event.target.value)}
+                    disabled={currencyDisable}
+                >
+
+                   {/* //Lets loop over currencyOptions, For looping here we are using map method using auto return implicit return  */}
+                    
+                       {
+currencyOptions.map((currency)=> (
+    <option key={currency} value={currency}>
+                        {currency}
+                        </option>
+))
+} 
+                
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox;
