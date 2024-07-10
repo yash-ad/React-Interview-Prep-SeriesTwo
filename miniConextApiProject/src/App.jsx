cb68aecf16c5f35a47f8cee6c63322dd4bdf0f21
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-07-10 22:13:05 +0530

    Notes added

--- miniConextApiProject/src/App.jsx
+++ miniConextApiProject/src/App.jsx
@@ -20,6 +20,12 @@ export default App
 
 //Notes:-
 
+///.Imp:-What is React context?
+//In React context is a feature it is a mechanism for global state management.it is used to sharing the data through the component tree without passing props down manually at every level to avoid props drilling.
+
+
+
+
 //1.CreateContext()
 //2.create a component context.Provider and set the value as a prop.
 //3.useContext ()
