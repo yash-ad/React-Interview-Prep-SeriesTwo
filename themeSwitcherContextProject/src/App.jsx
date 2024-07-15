89a3cad272ff82cfdd92db621da51a389313c710
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-07-15 13:21:33 +0530

    ThemeSwitcher project and practice completed

--- themeSwitcherContextProject/src/App.jsx
+++ themeSwitcherContextProject/src/App.jsx
@@ -1,37 +1,114 @@
 import { useState } from 'react'
-import ThemeProvider from './context/theme.js'
+import {ThemeProvider} from './context/theme.js'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
+import { useEffect } from 'react';
 
 function App() {
+//Lets define some functionalities that the context have provided.
+const[themeMode,setThemeMode] = useState("light");
 
-//Lets use the variable and function for functionalities over here which is created in the context.
-//Because we have an access from context.
-const[themeMode,setThemeMode]=useState("light");
+const darkTheme = ()=>{
+  setThemeMode("dark")
+}
 
 const lightTheme = ()=>{
   setThemeMode("light")
 }
-const darkTheme = ()=>{
-setThemeMode("dark")
-}
+
+
+//Actual change in theme in the App component because here we are providing the context for ThemeBtn and Card component:-
+ //For handling sideEffects in functional component we are using useEffect hook for manipulating the dom whenever there is something change in the UI , it re-renders the component with a dependency array.
+ useEffect(()=>{
+document.querySelector('body').classList.remove("light","dark")
+document.querySelector('body').classList.add(themeMode)
+ },[themeMode])
+
+
+
   return (
-   //Lets wrap themeContext because we want an access of 'Context' in the components.
-<ThemeProvider value={{themeMode , darkTheme , lightTheme}}>
+    //Extracted the values variable and methods from the context.
+<ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* themeBtn */}
+                        {/* ThemeBtn component */}
+                        <ThemeBtn/>
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       {/* Card */}
-                    </div>  
+                               {/* Card component */} 
+                               <Card/>
+                    </div>
                 </div>
             </div>
-
-</ThemeProvider>
-
+            </ThemeProvider>
 
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react'
+// import ThemeProvider from './context/theme.js'
+
+// function App() {
+
+// //Lets use the variable and function for functionalities over here which is created in the context.
+// //Because we have an access from context.
+// const[themeMode,setThemeMode]=useState("light");
+
+// const lightTheme = ()=>{
+//   setThemeMode("light")
+// }
+// const darkTheme = ()=>{
+// setThemeMode("dark")
+// }
+//   return (
+//    //Lets wrap themeContext because we want an access of 'Context' in the components.
+// <ThemeProvider value={{themeMode , darkTheme , lightTheme}}>
+// <div className="flex flex-wrap min-h-screen items-center">
+//                 <div className="w-full">
+//                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+//                         {/* themeBtn */}
+//                     </div>
+
+//                     <div className="w-full max-w-sm mx-auto">
+//                        {/* Card */}
+//                     </div>  
+//                 </div>
+//             </div>
+
+// </ThemeProvider>
+
+
+//   )
+// }
+
+// export default App
--- themeSwitcherContextProject/src/context/theme.js
+++ themeSwitcherContextProject/src/context/theme.js
@@ -1,21 +1,65 @@
+//Lets create a context :-
+import {createContext,useContext} from 'react'
 
-import {createContext,useContext} from 'react';
 
-
-//In the 'createContext()' we can take as a default value an object:-
+//Lets define some intialState and the methods and variables:-
 export const ThemeContext = createContext({
-    //We can pass any values from the context as such like variables and methods or functions
-    themeMode:"light",
-    darkTheme:()=>{},
-    lightTheme:()=>{},
-});
+themeMode:"light",
+darkTheme:()=>{},
+lightTheme:()=>{},
+}
+);
 
-
-//Also we can make a variable provider and also export from it within the same file.
+//Context provider
 export const ThemeProvider = ThemeContext.Provider;
 
-//Also we can make a custom hook function for using 'useContext()' hook:-
-//'useContext(createContext())'
+
+//Custom hook:-
 export default function useTheme(){
-    return useContext(ThemeContext)
+return useContext(ThemeContext);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import {createContext,useContext} from 'react';
+
+
+// //In the 'createContext()' we can take as a default value an object:-
+// export const ThemeContext = createContext({
+//     //We can pass any values from the context as such like variables and methods or functions
+//     themeMode:"light",
+//     darkTheme:()=>{},
+//     lightTheme:()=>{},
+// });
+
+
+// //Also we can make a variable provider and also export from it within the same file.
+// export const ThemeProvider = ThemeContext.Provider;
+
+// //Also we can make a custom hook function for using 'useContext()' hook:-
+// //'useContext(createContext())'
+// export default function useTheme(){
+//     return useContext(ThemeContext)
+// }
