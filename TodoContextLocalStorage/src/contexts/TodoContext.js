d7f7e0820c7d389a554680a2f85c849181dc3798
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-07-17 15:37:59 +0530

    New File and Folder added for TodoContextLocalStorage

---
+++ TodoContextLocalStorage/src/contexts/TodoContext.js
@@ -0,0 +1,28 @@
+import { createContext, useContext } from 'react'
+
+
+//createContext():-
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo msg",
+       completed:false,
+     }
+    ],
+    addTodo: (todo) => { },
+    updateTodo:(id,todo)=>{},
+    deleteTodo: (id) => { },
+    toggleComplete:(id)=>{ }
+});
+
+//context.Provider:-
+export const TodoProvider = TodoContext.Provider;
+
+//custom hook:-
+export const useTodo = () => {
+   return  useContext(TodoContext)
+};
+
+
+
