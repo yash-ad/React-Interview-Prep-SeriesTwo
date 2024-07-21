d782f1639b2e232a6513761baa01a4ea3d377e25
Yash Dandnaik <yashrajdandnaik@gmail.com>
2024-07-21 17:51:45 +0530

    code updated with fuctionalities

--- ReduxToolkitTodoProject/src/App.jsx
+++ ReduxToolkitTodoProject/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <h1 className="text-4xl text-center text-white mt-5">Redux-Toolkit Todo</h1>
-      <AddTodo/>
+      {/* <AddTodo/> */}
       <Todos/>
     </>
   )
--- ReduxToolkitTodoProject/src/components/AddTodo.jsx
+++ ReduxToolkitTodoProject/src/components/AddTodo.jsx
@@ -1,48 +1,61 @@
-import { useState } from 'react';
-import {addTodo} from '../features/todo/todoSlice'
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice';
 
-
-function AddTodo() {
+function AddTodo({ editId, editText, onUpdate }) {
   const [input, setInput] = useState('');
-
-  //'useDispatch()'hook provided by the 'react-redux' is used to dispatches an action to the Redux store.
+  const [isEditing, setIsEditing] = useState(false);
   const dispatch = useDispatch();
 
-  //Event handler for 'form'
-  const addTodoHandler = (event) => {
-    event.preventDefault()
-    //to check if an input field is empty
+  useEffect(() => {
+    if (editId !== null) {
+      setInput(editText);
+      setIsEditing(true);
+    } else {
+      setInput('');
+      setIsEditing(false);
+    }
+  }, [editId, editText]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
     if (input.trim() === '') {
-      alert('Kindly write some todo text !');
+      alert('Kindly write some todo text!');
       return;
     }
-    //Here to dispatch an action
-    //And here the action is to 'addTodo'
-    dispatch(addTodo(input))
-    setInput('')
-    
+    if (isEditing) {
+      onUpdate(input);
+    } else {
+      dispatch(addTodo(input));
+    }
+    setInput('');
+    setIsEditing(false);
   };
 
   return (
-    <div className=' flex justify-center'>
- <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
-      <input
-        type="text"
-        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="Enter a Todo..."
-        value={input}
-onChange={(event)=> setInput(event.target.value)}
-      />
-      <button
-        type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      >
-        Add Todo
-      </button>
+    <div className='flex justify-center'>
+      <form onSubmit={handleSubmit} className="space-x-3 mt-15">
+        <input
+          type="text"
+          className={`${isEditing ?"bg-gray-800 rounded border border-gray-700 focus:border-green-600 focus:ring-2 focus:ring-green-900 text-base outline-none text-gray-100 py-2 px-4 leading-8 transition-colors duration-200 ease-in-out" :"bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-2 px-4 leading-8 transition-colors duration-200 ease-in-out"}`}
+          placeholder="Enter a Todo..."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button
+          type="submit"
+          // {/* //Conditional rendering for button color */}
+      //     If isEditing is true, the button will have the classes for the "Update Todo" styling.
+      // If isEditing is false, the button will have the classes for the "Add Todo" styling.
+          className={`${isEditing ? 'text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg' 
+          : 'text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'}`}
+        >
+          {/* //Conditional rendering for button text */}
+          {isEditing ? 'Update Todo' : 'Add Todo'}
+        </button>
       </form>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default AddTodo
+export default AddTodo;
